d90fd49582e170c9e54d7ad0e933c3127c6fc771
Ahmed Masud <ahmed.masud@trustifier.com>
2017-01-23 01:00:18 -0500

    Several clean-ups:
    * Username can now be given on command-line
    * Directories to be owned during user creations can be specified on
    commandline
    * Creates /etc/default-docker-user file that contains the name of
      the user created (perhaps should append)
    
    WEIRDNESS/UNINTENDED FEATURE:
    	because this script moves the id_rsa.pub from /tmp
    	it has the side-effect of having to add id_rsa.pub to each
    	of the derived Dockerfiles, which is interesting
    	in that it allows us to add different ssh keys to each
    	default deployed image.
    
    	this "technique" may be used to further have build-level
    	changes from base image to derived image
    
    TODO:
    	* Add appropriate flags for chown / chmod
    	* Fix sudo group exists error

--- base-builder/start.sh
+++ base-builder/start.sh
@@ -1,14 +1,16 @@
 __create_user() {
-	local user=${1:-ansible}
+	local user=${1:-user}
+	shift
 	homedir=/home/${user}
-	groupadd sudo
+	groupadd --system sudo
 	useradd --groups sudo,wheel,root ${user}
 	usermod -p '*' ${user}
-	sed -i -e '/^[ \t]*%wheel/s/^/## /' -e '/NOPASSWD/s/^[ \t]*##*//' /etc/sudoers 
+	sed -i -e '/^[ \t]*%wheel/s/^/## /' -e '/NOPASSWD/s/^[ \t]*##*//' -e '/NOPASSWD/s/%wheel/%sudo/g' /etc/sudoers 
 	[ -d ${homedir}/.ssh ] || mkdir -m 700 -p ${homedir}/.ssh
 	mv /tmp/id_rsa.pub ${homedir}/.ssh/authorized_keys && chmod 600 ${homedir}/.ssh/authorized_keys
-	chown -R ansible.ansible /etc/ansible ${homedir}
+	chown -R ${user}:${user} ${homedir} "$@"
+	echo ${user} > /etc/default-docker-user
 }
 
 
-__create_user ansible
+__create_user "$1"
